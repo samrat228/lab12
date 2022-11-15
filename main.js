009cce54a31e59a3f559bbf76e879b662e64c5c8
Самрат Абенов <samrat.abenov@nu.edu.kz>
2022-11-15 23:54:40 +0600

    final push

--- main.js
+++ main.js
@@ -21,38 +21,40 @@ document.body.appendChild(p1)
 let ul1 = document.createElement("ul")
 document.body.appendChild(ul1);
 
-let li1 = document.createElement("li")
-li1.innerHTML = "find elements in the DOM (<strong>5 points</strong>);"
-li1.style.color = "green"
-ul1.appendChild(li1);
+li1 = document.createElement ("li")
+    li1.innerHTML = "find elements in the DOM <strong> (5 points)</strong>;"
+    li1.classList = "odd"
+    ul1.appendChild(li1)
 
-let li2 = document.createElement("li")
-li2.innerHTML = "create/add/remove elements (<strong>5 points</strong>);"
-li2.style.color = "purple"
-ul1.appendChild(li2);
+    li2 = document.createElement ("li")
+    li2.innerHTML = "create/add/remove elements <strong> (5 points)</strong>;"
+    li2.classList = "even"
+    ul1.appendChild(li2)
 
-let li3 = document.createElement("li")
-li3.innerHTML = "change content of the elements (<strong>5 points</strong>);"
-li3.style.color = "green"
-ul1.appendChild(li3);
+    li3 = document.createElement ("li")
+    li3.innerHTML = "change content of the elements <strong> (5 points)</strong>;"
+    li3.classList = "odd"
+    ul1.appendChild(li3)
 
-let li4 = document.createElement("li")
-li4.innerHTML = "change styles of the elements (<strong>5 points</strong>);"
-li4.style.color = "purple"
-ul1.appendChild(li4);
+    li4 = document.createElement ("li")
+    li4.innerHTML = "change styles of the elements <strong> (5 points)</strong>;"
+    li4.classList = "even"
+    ul1.appendChild(li4)
 
-let li5 = document.createElement("li")
-li5.innerHTML = "change attributes of the elements (<strong>5 points</strong>);"
-li5.style.color = "green"
-ul1.appendChild(li5);
+    li5 = document.createElement ("li")
+    li5.innerHTML = "change attributes of the elements <strong> (5 points)</strong>;"
+    li5.classList = "odd"
+    ul1.appendChild(li5)
 
-let li6 = document.createElement("li")
-li6.innerHTML = "change classes of the elements (<strong>5 points</strong>)."
-li6.style.color = "purple"
-ul1.appendChild(li6);
+    li6 = document.createElement ("li")
+    li6.innerHTML = "change classes of the elements <strong> (5 points)</strong>;"
+    li6.classList = "even"
+    ul1.appendChild(li6)    
+
+document.body.appendChild(ul1)
 
 let a = document.createElement("a");
-a.innerText = "link";
+a.innerText = "link.";
 a.setAttribute("href", "https://github.com/yessen/nu_web_programming/tree/main/week12");
 let p2 = document.createElement("p");
 p2.innerHTML = "Basic necessary code can be found in the supplementary materials to the Lecture 12 via this ";
@@ -75,41 +77,54 @@ document.body.appendChild(p3);
 let ul2 = document.createElement("ul")
 document.body.appendChild(ul2);
 
-let li7 = document.createElement("li")
-li7.innerHTML = "Create a new repository on Github, named <strong>lab12 (1 point)</strong>."
-li7.style.color = "green"
-ul2.appendChild(li7);
+    li1 = document.createElement ("li")
+    li1.innerHTML = "Create a new repository on Github, named <strong> lab12 (1 point).</strong>"
+    li1.classList = "odd"
+    ul2.appendChild(li1)
 
-let li8 = document.createElement("li")
-li8.innerHTML = "Clone this repository to your local machine and work inside it."
-li8.style.color = "purple"
-ul2.appendChild(li8);
+    li2 = document.createElement ("li")
+    li2.innerHTML = "Clone this repository to your local machine and work inside it."
+    li2.classList = "even"
+    ul2.appendChild(li2)
 
-let li9 = document.createElement("li")
-li9.innerHTML = "Create a new HTML file, called <b>index.html</b>, which has only one &lt;h1&gt; tag with 'Hello, World!' message (<b>1 point</b>)."
-li9.style.color = "green"
-ul2.appendChild(li9);
+    li3 = document.createElement ("li")
+    li3.innerHTML = "Create a new HTML file, called <strong> index.html</strong>, which has only one &lt;h1&gt; tag with &#34;Hello, World!&#34; message <strong>(1 point).</strong>"
+    li3.classList = "odd"
+    ul2.appendChild(li3)
 
-let li10 = document.createElement("li")
-li10.innerHTML = "Create a new JavaScript file, called <b>main.js</b>, which must contain your program (assignment) described above (<b>1 point</b>)."
-li10.style.color = "purple"
-ul2.appendChild(li10);
+    li4 = document.createElement ("li")
+    li4.innerHTML = "Create a new JavaScript file, called <strong> main.js</strong>, which must contain your program (assignment) described above (<strong>1 point</strong>)."
+    li4.classList = "even"
+    ul2.appendChild(li4)
 
-let li11 = document.createElement("li")
-li11.innerHTML = "Link this <b>main.js</b> file to your <b>index.html</b> file (Note: place your script at the end of the <b>body</b> section)."
-li11.style.color = "green"
-ul2.appendChild(li11); 
+    li5 = document.createElement ("li")
+    li5.innerHTML = "Link this <strong> main.js </strong> file to your <strong> index.html </strong> file (Note: place your script at the end of the <strong> body </strong> section)."
+    li5.classList = "odd"
+    ul2.appendChild(li5)
 
-let li12 = document.createElement("li")
-li12.innerHTML = "Write a JavaScript program in <b>main.js</b> to make your <b>index.html</b> look identical to this HTML file (<b>5 points</b>)."
-li12.style.color = "purple"
-ul2.appendChild(li12);
+    li6 = document.createElement ("li")
+    li6.innerHTML = "Write a JavaScript program in <strong> main.js </strong> to make your <strong> index.html </strong> look identical to this HTML file (<strong>5 points</strong>)."
+    li6.classList = "even"
+    ul2.appendChild(li6)    
 
-let li13 = document.createElement("li")
-li13.innerHTML = "After you finish your work, submit it to the Github (<b>2 points</b>)."
-li13.style.color = "green"
-ul2.appendChild(li13);
+    li7 = document.createElement ("li")
+    li7.innerHTML = "After you finish your work, submit it to the Github (<strong>2 points</strong>)."
+    li7.classList = "even"
+    ul2.appendChild(li7)  
+
+document.body.appendChild(ul2)
 
 let hr4 = document.createElement("hr")
 console.log(hr4);
 document.body.appendChild(hr4);
+
+let x = document.getElementsByClassName("odd");
+console.log(x); 
+let y = document.getElementsByClassName("even");
+console.log(y);
+for (let i=0; i<y.length;i++) {
+	y[i].style.color = 'purple'
+    x[i].style.color = 'green'
+}
+
+
